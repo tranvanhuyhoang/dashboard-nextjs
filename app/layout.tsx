8cf8d394a491a12a36763c582937c5dd1980b515
hoangtvh <tranvanhuyhoang@gmail.com>
2024-05-16 20:48:07 +0700

    init source nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { BeVietnamPro } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={BeVietnamPro.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Be_Vietnam_Pro, Lusitana } from 'next/font/google';
+
+export const BeVietnamPro = Be_Vietnam_Pro({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
